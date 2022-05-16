6b37799fce027a765f88f1ace2dbe38c67a3c3bd
pradnyeshgaonkar123 <pradnyeshgaonkar106@gmail.com>
2022-05-16 18:13:49 +0530

    Added missing annotation

--- src/main/java/com/usermanagement/controller/UserController.java
+++ src/main/java/com/usermanagement/controller/UserController.java
@@ -3,6 +3,8 @@ package com.usermanagement.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,11 +15,12 @@ import com.usermanagement.service.UserServiceImpl;
 @RestController
 public class UserController {
 
+	@Autowired
 	private UserServiceImpl userServiceImpl;
 	
 	
 	@RequestMapping(value = "/save-user", method = RequestMethod.POST)
-	public User saveUser(User user) {
+	public User saveUser(@RequestBody User user) {
 		return userServiceImpl.saveUser(user);
 	}
 	
